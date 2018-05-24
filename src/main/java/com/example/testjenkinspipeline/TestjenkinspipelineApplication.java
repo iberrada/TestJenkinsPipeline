1f03c20efd80ea362dc58acb8196a29a18ceeee2
Ismail BERRADA <iberrada@sii.fr>
2018-05-24 10:58:00 +0200

    Initial Commit

---
+++ src/main/java/com/example/testjenkinspipeline/TestjenkinspipelineApplication.java
@@ -0,0 +1,12 @@
+package com.example.testjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjenkinspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjenkinspipelineApplication.class, args);
+	}
+}
